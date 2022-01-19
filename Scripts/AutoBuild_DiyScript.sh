b1606224b789421e2c3447d6379c69a3af61a438
yoyobbs <79379450@qq.com>
2022-01-19 22:30:31 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -6,8 +6,8 @@ Firmware_Diy_Core() {
 
 	Author=AUTO
 	Author_URL=AUTO
-	Default_IP="192.168.1.1"
-	Banner_Message="Powered by AutoBuild-Actions"
+	Default_IP="192.168.123.1"
+	Banner_Message="Powered by Brian"
 
 	Short_Firmware_Date=true
 	Checkout_Virtual_Images=false
